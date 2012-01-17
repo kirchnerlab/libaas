586a5286ac89d6846a6d951cc4441a818f5e061a
Mathias Wilhelm <mw@wilhelmonline.com>
2012-01-17 19:15:32 +0100

    added basic documentation for libaas::errors

--- include/libaas/Error.hpp
+++ include/libaas/Error.hpp
@@ -19,23 +19,36 @@
 namespace libaas {
 namespace errors {
 
+/**Base class for all libaas exceptions.
+ */
 class Exception : public std::exception
 {
 public:
+    /**Default constructor.
+     * @param[in] message The reason/description of the exception
+     */
     Exception(const libaas::Char* message)
     {
         what_ = message;
     }
 
+    /**Default constructor.
+     * @param[in] message The reason/description of the exception
+     */
     Exception(const libaas::String& message)
     {
         what_ = message;
     }
 
+    /**Descructor.
+     */
     virtual ~Exception() throw ()
     {
     }
 
+    /**Returns the reason/description of the exception
+     * @returns Reason/description of the exception.
+     */
     virtual const char* what() const throw ()
     {
         return what_.c_str();
@@ -50,22 +63,29 @@ protected:
 };
 // class Exception
 
-/**
- * invalid external input or code bugs
+/**A LogicError is used for invalid external input or code bugs.
  */
 class LogicError : public libaas::errors::Exception
 {
 public:
+    /**Default constructor.
+     * @param[in] message The reason/description of the exception
+     */
     explicit LogicError(const char* message) :
             Exception(message)
     {
     }
 
+    /**Default constructor.
+     * @param[in] message The reason/description of the exception
+     */
     explicit LogicError(const libaas::String& message) :
             Exception(message)
     {
     }
 
+    /**Desctructor.
+     */
     virtual ~LogicError() throw ()
     {
     }
@@ -73,22 +93,29 @@ public:
 };
 // class LogicError
 
-/**
- * Runtime errors caused by not acquirable system resources
+/**Runtime errors are caused by not acquirable system resources or unexpected failures.
  */
 class RuntimeError : public libaas::errors::Exception
 {
 public:
+    /**Default constructor.
+     * @param[in] message The reason/description of the exception
+     */
     explicit RuntimeError(const char* message) :
             Exception(message)
     {
     }
 
+    /**Default constructor.
+     * @param[in] message The reason/description of the exception
+     */
     explicit RuntimeError(const libaas::String& message) :
             Exception(message)
     {
     }
 
+    /**Destructor.
+     */
     virtual ~RuntimeError() throw ()
     {
     }
@@ -96,19 +123,29 @@ public:
 };
 // class RuntimeError
 
+/**Out of range exceptions.
+ */
 class OutOfRange : public LogicError
 {
 public:
+    /**Default constructor.
+     * @param[in] message The reason/description of the exception
+     */
     explicit OutOfRange(const char* message) :
             LogicError(message)
     {
     }
 
+    /**Default constructor.
+     * @param[in] message The reason/description of the exception
+     */
     explicit OutOfRange(const libaas::String& message) :
             LogicError(message)
     {
     }
 
+    /**Destructor.
+     */
     virtual ~OutOfRange() throw ()
     {
     }
@@ -116,6 +153,11 @@ public:
 };
 // class OutOfRange
 
+/**Throws a LogicError in case the predicate is not true.
+ * @param[in] predicate Predicate
+ * @param[in] message Message
+ * @throws Throws a LogicError in case the predicate is not true
+ */
 inline void throw_logic_error(libaas::Bool predicate, const char* message)
 {
     if (!predicate) {
@@ -123,6 +165,11 @@ inline void throw_logic_error(libaas::Bool predicate, const char* message)
     }
 }
 
+/**Throws an OutOfRange exceptions in case the predicate is not true.
+ * @param[in] predicate
+ * @param[in] message
+ * @throws Throws an OutOfRange exception in case the predicate is not true.
+ */
 inline void throw_outofrange_error(libaas::Bool predicate, const char* message)
 {
     if (!predicate) {
@@ -130,12 +177,22 @@ inline void throw_outofrange_error(libaas::Bool predicate, const char* message)
     }
 }
 
+/**Convenience definition to easily throw a LogicError.
+ */
 #define libaas_logic_error_cond(PREDICATE, MESSAGE) libaas::errors::throw_logic_error(PREDICATE, MESSAGE)
+/**Convenience definition to easily throw a LogicError.
+ */
 #define libaas_logic_error(MESSAGE) throw libaas::errors::LogicError(MESSAGE)
 
+/**Convenience definition to easily throw an OutOfRange.
+ */
 #define libaas_outofrange_cond(PREDICATE, MESSAGE) libaas::errors::throw_outofrange_error(PREDICATE, MESSAGE)
+/**Convenience definition to easily throw an OutOfRange.
+ */
 #define libaas_outofrange(MESSAGE) throw libaas::errors::OutOfRange(MESSAGE)
 
+/**Convenience definition to easily throw a RunTimeError.
+ */
 #define libaas_fail(MESSAGE) throw libaas::errors::RuntimeError (MESSAGE)
 
 } // namespace errors
